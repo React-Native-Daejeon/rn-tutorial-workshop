bb76a9493a2d04a1515d7bea385f4c15906dedfb
seyeon-kim <ha7198ha@kaist.ac.kr>
2020-08-16 17:17:35 +0900

    Note 추가하고 Note들의 제목 MainScreen에 띄워보기

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
+import {connect} from 'react-redux';
 
 /**
  * Main 화면을 담당할 MainScreen 입니다.
  */
+
+const mapStateToProps = (state) => {
+  return {
+    notes: state.app_reducer.notes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
 class MainScreen extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const notes = this.props.notes;
     return (
       <SafeAreaView>
         <Text>MainScreen</Text>
@@ -19,9 +32,12 @@ class MainScreen extends React.Component {
           }}
           title="Go to Note"
         />
+        {notes.map((note, index) => {
+          return <Text key={`${note}::${index}`}>{note.title}</Text>;
+        })}
       </SafeAreaView>
     );
   }
 }
 
-export default MainScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
--- src/screens/NoteScreen.js
+++ src/screens/NoteScreen.js
@@ -1,18 +1,67 @@
 import React from 'react';
-import {SafeAreaView, Text, Button} from 'react-native';
+import {SafeAreaView, Text, Button, TextInput} from 'react-native';
+import {connect} from 'react-redux';
+import {newNote} from '../actions/actions';
 
 /**
  * Note(Memo) 화면을 담당할 NoteScreen 입니다.
  */
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    makeNewNote: (note) => dispatch(newNote(note)),
+  };
+};
+
 class NoteScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: '',
+      body: '',
+    };
+  }
+
+  onChangeTitle(text) {
+    this.setState({title: text});
+  }
+
+  onChangeBody(text) {
+    this.setState({body: text});
+  }
+
+  onSubmitNote() {
+    this.props.makeNewNote({title: this.state.title, body: this.state.body});
   }
 
   render() {
     return (
       <SafeAreaView>
         <Text>NoteScreen</Text>
+        <Text>title</Text>
+        <TextInput
+          value={this.state.title}
+          onChangeText={(text) => {
+            this.onChangeTitle(text);
+          }}
+        />
+        <Text>body</Text>
+        <TextInput
+          value={this.state.body}
+          onChangeText={(text) => {
+            this.onChangeBody(text);
+          }}
+        />
+        <Button
+          onPress={() => {
+            this.onSubmitNote();
+          }}
+          title="Submit"
+        />
         <Button
           onPress={() => {
             this.props.navigation.goBack();
@@ -24,4 +73,4 @@ class NoteScreen extends React.Component {
   }
 }
 
-export default NoteScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(NoteScreen);
